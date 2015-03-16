a78b0886ad764c25aebab9c3fc7462ff4efbd2fa
TheM0zart <m0zart1337@hotmail.com>
2015-03-16 10:14:44 +0100

    new TCPread functions

--- app/src/main/java/com/example/daniel/festoar/RobotThread.java
+++ app/src/main/java/com/example/daniel/festoar/RobotThread.java
@@ -10,12 +10,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.io.ObjectInputStream;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
 
 
 public class RobotThread extends AsyncTask<Object, Void, RobotData> { //<args, state(?), retval>
@@ -32,6 +34,30 @@ public class RobotThread extends AsyncTask<Object, Void, RobotData> { //<args, s
 
         RobotData robData = new RobotData(); //declare as class private variable ?
         mParentActivity = (MainActivity) params[0];
+        byte[] data = new byte[maxBytesRead];
+
+        /* Read TCP Data */
+        //data = readTCPSocketByteArray(params);
+        data = readTCPSocketByByte(params);
+
+        /* Parse XML String to RobotData */
+        try {
+            robData = (RobotData) new XMLParser().XMLStringToRobotData(new String(data, "ISO-8859-1")); //use .clone() ?
+        } catch (UnsupportedEncodingException e){
+            //System.out.println("Exception thrown  :" + e);
+            Log.i(tag, e.toString());
+        }
+
+        return robData;
+    }
+
+    protected void onPostExecute(RobotData newData) {
+        mParentActivity.updateRobotData(newData);
+        mParentActivity.updateDispData();
+    }
+
+    public byte[] readTCPSocketByteArray(Object... params){
+
         InputStream robInStream;
         byte[] data = new byte[maxBytesRead];
         int bRead = -1;
@@ -42,60 +68,95 @@ public class RobotThread extends AsyncTask<Object, Void, RobotData> { //<args, s
         if(!cliSock.isConnected()) {
             try {
                 cliSock.connect(hostSock, maxTimeout); //connect to server
-                robData.robOnline = true;
                 robInStream = cliSock.getInputStream();
+                /* Read Socket */
                 bRead = robInStream.read(data); //read data
+                //debug
                 Log.i(tag, "bytes read: "+bRead+"\n");
-                if(bRead > 0){
-                    /* Parse RobotData from XML String */
-                    //try{
-                        robData = (RobotData) new XMLParser().XMLStringToRobotData(new String(data, "ISO-8859-1")); //use .clone() ?
-                        //Log.i(tag, robData.toString());
-                    /*}catch(CloneNotSupportedException e){
-                        //System.out.println("Exception thrown  :" + e);
-                        Log.i(tag, e.toString());
-                        robData.robOnline = false;
-                    }*/
-                    //robData.RobotName = new String(data, "ISO-8859-1").trim(); //working charsets: US-ASCII, ISO-8859-1, UTF-8
-                    //robData.robOnline = true; //connection successful
-                    //Log.i(tag, Integer.toString(robData.RobotName.length()));
-                    //Log.i(tag, robData.RobotName);
-                    /*Log.i(tag, new String(data, "US-ASCII"));
-                    Log.i(tag, new String(data, "ISO-8859-1"));
-                    Log.i(tag, new String(data, "UTF-8"));
-                    Log.i(tag, new String(data, "UTF-16BE"));
-                    Log.i(tag, new String(data, "UTF-16LE"));
-                    Log.i(tag, new String(data, "UTF-16"));*/
-                    /*Log.i(tag, "-----------------------------");
-                    Log.i(tag, Integer.toString(bRead)+" bytes read: ");
-                    String recvInts = new String();
-                    Log.i(tag, new String(data, "ISO-8859-1").trim());*/
-                    /*for(int x = 0; x < maxBytesRead; x++) {
-                        recvInts+=Integer.toString(data[x]);
-                    }
-                    Log.i(tag, recvInts);*/
-                    /*
-                    Log.i(tag, robData.RobotName.getBytes("US-ASCII").toString());
-                    Log.i(tag, robData.RobotName.getBytes("ISO-8859-1").toString());
-                    Log.i(tag, robData.RobotName.getBytes("UTF-8").toString());
-                    Log.i(tag, robData.RobotName.getBytes("UTF-16BE").toString());
-                    Log.i(tag, robData.RobotName.getBytes("UTF-16LE").toString());
-                    Log.i(tag, robData.RobotName.getBytes("UTF-16").toString());*/
-
-                }
+                debug_printAllCharSets(data, false);
                 cliSock.close();
             } catch (IOException e) {
                 //System.out.println("Exception thrown  :" + e);
                 Log.i(tag, e.toString());
-                robData.robOnline = false;
+                return null;
+            } catch (NullPointerException e){
+                //System.out.println("Exception thrown  :" + e);
+                Log.i(tag, e.toString());
+                return null;
             }
         }
-        return robData;
+        if(bRead <= 0) return null;
+        return data;
     }
 
-    protected void onPostExecute(RobotData newData) {
-        mParentActivity.updateRobotData(newData);
-        mParentActivity.updateDispData();
+    public byte[] readTCPSocketByByte(Object... params){
+
+        InputStream robInStream;
+        byte[] data = new byte[maxBytesRead];
+        int i = 0;
+        int bRead = -1;
+
+        /* Socket*/
+        InetSocketAddress hostSock = (InetSocketAddress) params[1];
+        Socket cliSock = new Socket(); //create socket
+        if(!cliSock.isConnected()) {
+            try {
+                cliSock.connect(hostSock, maxTimeout); //connect to server
+                robInStream = cliSock.getInputStream();
+                /* Read Socket */
+                do{
+                    bRead = robInStream.read(); //read data
+                    data[i] = (byte) bRead;
+                    i++;
+                }while(bRead != -1);
+                //debug
+                Log.i(tag, "bytes read: "+bRead+"\n");
+                debug_printAllCharSets(data, false);
+                cliSock.close();
+            } catch (IOException e) {
+                //System.out.println("Exception thrown  :" + e);
+                Log.i(tag, e.toString());
+                return null;
+            } catch (NullPointerException e){
+                //System.out.println("Exception thrown  :" + e);
+                Log.i(tag, e.toString());
+                return null;
+            }
+        }
+        if(bRead <= 0) return null;
+        return data;
+    }
+
+    /* DEBUG */
+
+    public void debug_printAllCharSets(byte[] data, boolean trimmed){ //working charsets: US-ASCII, ISO-8859-1, UTF-8
+        if(trimmed){
+            try{
+                Log.i(tag, "Length: "+data.length+"\n");
+                Log.i(tag, new String(data, "US-ASCII").trim());
+                Log.i(tag, new String(data, "ISO-8859-1").trim());
+                Log.i(tag, new String(data, "UTF-8").trim());
+                Log.i(tag, new String(data, "UTF-16BE").trim());
+                Log.i(tag, new String(data, "UTF-16LE").trim());
+                Log.i(tag, new String(data, "UTF-16").trim());
+            }catch(UnsupportedEncodingException e) {
+                //System.out.println("Exception thrown  :" + e);
+                Log.i(tag, e.toString());
+            }
+        }else{
+            try{
+                Log.i(tag, "Length: "+data.length+"\n");
+                Log.i(tag, new String(data, "US-ASCII"));
+                Log.i(tag, new String(data, "ISO-8859-1"));
+                Log.i(tag, new String(data, "UTF-8"));
+                Log.i(tag, new String(data, "UTF-16BE"));
+                Log.i(tag, new String(data, "UTF-16LE"));
+                Log.i(tag, new String(data, "UTF-16"));
+            }catch(UnsupportedEncodingException e) {
+                //System.out.println("Exception thrown  :" + e);
+                Log.i(tag, e.toString());
+            }
+        }
     }
 
 }
